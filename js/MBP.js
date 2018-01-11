f2cc1ffed86fc3f68364f0d645072f5a25039339
GlenLexry <glenwan@gmail.com>
2018-01-11 19:17:31 +0800

    Signed-off-by: GlenLexry <glenwan@gmail.com>

--- js/MBP.js
+++ js/MBP.js
@@ -5,6 +5,6 @@ var img = new Image();
    img.src = '../img/Asset-10.svg';
 
 document.querySelector('.search-icon').addEventListener('click' ,function() {
-  var searchbar = document.querySelector('.search-bar').innerHTML = '<div class="input-group"><span class="input-group-addon btn">Search</span><input type="text" class="form-control"></div>';
+  var searchbar = document.querySelector('.search-bar').innerHTML = '<div class="input-group"><input type="text" class="form-control" placeholder="Search"></div>';
   var hideSearchIcon = document.querySelector('.search-icon').style.display = 'none';
 });
